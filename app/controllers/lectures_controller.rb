112c01d4267a7e621684751d399eb4e1d7bbe07e
mognedy <mognedy@Gmail.com>
2017-05-09 20:14:23 +0200

    ckeditor_rails added to lectures content

--- app/controllers/lectures_controller.rb
+++ app/controllers/lectures_controller.rb
@@ -3,7 +3,7 @@ before_filter :authenticate_user!
   private
 
     def lecture_params
-      params.require(:lecture).permit(:title, :content, :attachment, :course_id)
+      params.require(:lecture).permit(:title, :content, :attachment, :course_id, :remove_attachment)
     end
     #  @default_lecture = Lecture.find(lecture_params[:course_id])
 end
