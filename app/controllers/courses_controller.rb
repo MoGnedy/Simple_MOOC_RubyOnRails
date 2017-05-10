c5844fa67fed104bef3421c580195c5d15eb96e4
mognedy <mognedy@Gmail.com>
2017-05-10 14:39:50 +0200

    some helpers and style added

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,8 +1,17 @@
 class CoursesController < InheritedResources::Base
-load_and_authorize_resource
+  load_and_authorize_resource
+  before_action :set_course, only: [:show]
+  before_action :get_all_lectures, only: [:show]
+  def get_all_lectures
+    @lectures = @course.lectures.all
+  end
+
   private
+    def set_course
+      @course = Course.find(params[:id])
+    end
 
     def course_params
-      params.require(:course).permit(:title, :user_id)
+        params.require(:course).permit(:title, :user_id)
     end
 end
--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,9 +3,14 @@ class Ability
 
   def initialize(user)
     user ||= User.new # guest user (not logged in)
+
+#admin privilages
     if user.admin?
       can :manage, :all
+
+#instructor privilages
     elsif user.instructor?
+      can :show, User
       can :read, [Course, Lecture]
       can :upvote, Lecture
       can :downvote, Lecture
@@ -19,38 +24,27 @@ class Ability
       can :manage, Comment do |comment|
       comment.try(:user) == user
     end
+    can :manage, User do |usr|
+      usr == user
+    end
+
+
+#student privilages
     elsif user.student?
+      can :show, User
+      can :manage, User do |usr|
+        usr.try(:usr) == user
+      end
       can :read, [Course, Lecture]
       can :upvote, Lecture
       can :downvote, Lecture
+      can :manage, User do |usr|
+        usr == user
+      end
+
+#guest
     else
       can :read, :Course
     end
-    # Define abilities for the passed in user here. For example:
-    #
-    #   user ||= User.new # guest user (not logged in)
-    #   if user.admin?
-    #     can :manage, :all
-    #   else
-    #     can :read, :all
-    #   end
-    #
-    # The first argument to `can` is the action you are giving the user
-    # permission to do.
-    # If you pass :manage it will apply to every action. Other common actions
-    # here are :read, :create, :update and :destroy.
-    #
-    # The second argument is the resource the user can perform the action on.
-    # If you pass :all it will apply to every resource. Otherwise pass a Ruby
-    # class of the resource.
-    #
-    # The third argument is an optional hash of conditions to further filter the
-    # objects.
-    # For example, here the user can only update published articles.
-    #
-    #   can :update, Article, :published => true
-    #
-    # See the wiki for details:
-    # https://github.com/ryanb/cancan/wiki/Defining-Abilities
   end
 end
